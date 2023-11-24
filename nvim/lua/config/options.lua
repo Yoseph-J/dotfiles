a5409790aede1781c57489816c4f28ec545be5cd
Yoseph Jeong <yoseph.jeong@outlook.com>
2023-11-24 11:03:36 +0900

    Added relative line number options to nvim/lua/config/options.lua

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,8 +1,14 @@
+-- modeline options
 vim.opt.modeline = true
 vim.opt.modelines = 5
 
+-- tab options
 vim.opt.smarttab = true
 vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
+
+-- line number options
+vim.opt.number = true
+vim.opt.relativenumber = true
