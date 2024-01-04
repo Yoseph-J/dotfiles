09bce72b60712ab53cae165f52efe0cff9e093de
Yoseph Jeong <yoseph.jeong@outlook.com>
2024-01-04 16:39:32 +0900

    everythin

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -34,4 +34,23 @@ return {
     end,
 
   },
+  {
+    -- Theme inspired by Atom
+    'navarasu/onedark.nvim',
+    lazy = true,
+    priority = 1000,
+  },
+  {
+    -- Set lualine as statusline
+    'nvim-lualine/lualine.nvim',
+    -- See `:help lualine.txt`
+    opts = {
+      options = {
+        icons_enabled = false,
+        theme = 'onedark',
+        component_separators = '|',
+        section_separators = '',
+      },
+    },
+  },
 }
